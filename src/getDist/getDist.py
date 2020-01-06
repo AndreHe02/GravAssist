4ca2713a8330e0c2cd2f7b2e3a1e6ec853adf9d3
aelu419 <alantao20011015@outlook.com>
2020-01-06 23:38:48 +0800

    correction on getDist

--- src/getDist/getDist.py
+++ src/getDist/getDist.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 from builtins import input
-from datetime import datetime, timedelta
+from datetime import datetime
 import spiceypy
 
 #test function to see if spiceypy is installed correctly
@@ -11,7 +11,7 @@ def print_ver():
 
 #get relative position and velocity of target from POV of observer at TIME, using correction mode mode
 def getsta(target, TIME, mode = "LT+S", observer = "SOLAR SYSTEM BARYCENTER"):
-    ##the following are from https://spiceypy.readthedocs.io/en/master/remote_sensing.html
+    # https://spiceypy.readthedocs.io/en/master/remote_sensing.html
     #
     # Local parameters
     #
@@ -29,7 +29,7 @@ def getsta(target, TIME, mode = "LT+S", observer = "SOLAR SYSTEM BARYCENTER"):
     #
     #Prompt the user for the input time string.
     #
-    utctim = TIME
+    utctim = parseDate(TIME)
 
     print( 'Converting UTC Time: {:s}'.format(utctim)  )
 
@@ -41,8 +41,8 @@ def getsta(target, TIME, mode = "LT+S", observer = "SOLAR SYSTEM BARYCENTER"):
     print( '   ET seconds past J2000: {:16.3f}'.format(et) )
 
     #
-    # Compute the apparent state of Phoebe as seen from
-    # CASSINI in the J2000 frame.  All of the ephemeris
+    # Compute the apparent state of target as seem from
+    # observer in the J2000 frame.  All of the ephemeris
     # readers return states in units of kilometers and
     # kilometers per second.
     #
@@ -53,12 +53,14 @@ def getsta(target, TIME, mode = "LT+S", observer = "SOLAR SYSTEM BARYCENTER"):
            'from SSB (0) in the J2000\n'
            '      frame (km, km/s):'              )
 
-    print( '      X = {:16.3f}'.format(state[0])       )
-    print( '      Y = {:16.3f}'.format(state[1])       )
-    print( '      Z = {:16.3f}'.format(state[2])       )
-    print( '     VX = {:16.3f}'.format(state[3])       )
-    print( '     VY = {:16.3f}'.format(state[4])       )
-    print( '     VZ = {:16.3f}'.format(state[5])       )
+    #print( '      X = {:16.3f}'.format(state[0])       )
+    #print( '      Y = {:16.3f}'.format(state[1])       )
+    #print( '      Z = {:16.3f}'.format(state[2])       )
+    #print( '     VX = {:16.3f}'.format(state[3])       )
+    #print( '     VY = {:16.3f}'.format(state[4])       )
+    #print( '     VZ = {:16.3f}'.format(state[5])       )
+
+    return state
 
 def below10(num):
     if(num<10):
@@ -72,45 +74,3 @@ def formatDateNumElement(num):
 #parses 1234.12.12 into '1234 mon 12 12:34:56"
 def parseDate(dateObj):
     return dateObj.strftime("%Y %b %d %H:%M:%S").lower()
-
-
-if __name__ == '__main__':
-    #s = "2019 jun 11 00:00:00"
-
-    #getsta(target, TIME, mode, observer):
-    #mode is default to be LT+S, it is a method they use to correct data, which is recommended in the tutorial w/ Cassini
-    #observer is default to be SSB (0), solar system barycenter
-    #time is required (s = "2019 jun 11 00:00:00")
-    #target is required
-
-    tgt = 'MARS BARYCENTER' #this example uses Mars Barycenter (4)
-
-    #for i in range(0,59):
-        #print(s.format(below10(i),i),end="")
-        #getsta(target = tgt, TIME = s.format(below10(i),i))
-
-    print("date format: yyyy.mm.dd --no spaces, all numbers")
-    s1 = input("starting date:")
-    s2 = input("ending date:")
-    print("increment is 8hrs at default, change in prgm")
-
-    dateStart = datetime(int(s1[0:4]), int(s1[5:7]), int(s1[8:10]))
-    dateEnd = datetime(int(s2[0:4]), int(s2[5:7]), int(s2[8:10]))
-    dateTemp = dateStart
-
-    dateLs = []
-    tDlt = timedelta(days = 8)
-
-    #print(dateStart)
-    while dateTemp < dateEnd:
-        dateLs.append(parseDate(dateTemp))
-        dateTemp += tDlt
-        #print(dateTemp)
-
-    print (dateLs)
-
-    print('---------------------------')
-
-    for i in dateLs:
-        getsta(tgt, i)
-    
