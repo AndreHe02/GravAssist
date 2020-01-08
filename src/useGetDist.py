7ca6977a9430bd0e6e2af1afe9f82e8e1b004173
aelu419 <alantao20011015@outlook.com>
2020-01-08 08:54:52 +0800

    better method

--- src/useGetDist.py
+++ src/useGetDist.py
@@ -11,6 +11,93 @@ plt.style.use('seaborn-whitegrid')
 
 import numpy as np
 
+np.seterr('raise')
+
+#graph hyperbolic trajectory
+def getExitState(target, TIME, state0):
+    #os.chdir("./SPICE/")
+
+    #constant storing the planet's basic info: [radius(km), gm, SOI radius(km)]
+    plntConst = getDist.getConst(target,TIME)
+
+    #unpack & init
+    pos = complex(state0[0],state0[1])
+    #posLog = []
+    vel = complex(state0[2],state0[3])
+    gm, soi = plntConst
+
+    #print("r0:",pos)
+    #print("v:",vel)
+    #print("gm:",gm)
+    #print("SoI:",soi)
+
+    dist = abs(pos)
+    #print("r:",dist)
+
+    #see Basyal
+    semiMajor = 1/(2/dist - abs(vel)**2/gm)
+    specificAngMomentum = dist * abs(vel)
+    specificOrbitalEnergy = abs(vel)**2/2-gm/dist
+    trm = dist**2
+    trm1 = abs(vel)**2
+    #print("trm:", trm)
+    #print("trm1:",trm1)
+    eccen = np.sqrt(1 + specificOrbitalEnergy * trm * trm1 / gm ** 2)
+    #eccentricity determines shape, hyperbola eccen>1
+    if eccen<=1:
+        return False
+
+    #print("a:",semiMajor)
+    #print("e:",eccen)
+
+    trueAnom = np.arccos((semiMajor * (1 - eccen**2)-dist)/(eccen*dist))
+
+    #correct quadrant of arccos for trueAnom, using the sign of flightPathAngle
+    flightPathAngle = np.arctan(eccen * np.sin(trueAnom) / (1+eccen*np.cos(trueAnom)))
+    if (flightPathAngle > 0 and trueAnom < 0) or (flightPathAngle < 0 and trueAnom > 0):
+        trueAnom *= -1
+    trueAnom -= np.pi
+    #print("initAnom:",trueAnom)
+    #actual angle = angle of position vector to the hofizontal axis, not the axis
+    #of the hyperbola
+
+    actualAngle = findAngle(pos.real, pos.imag)
+    #print("entranceAngle:",actualAngle)
+
+    exitAngle = actualAngle + 2 * trueAnom
+
+    #print("exitAngle:",exitAngle)
+    c = semiMajor * eccen
+    centerAxisAngle = actualAngle + trueAnom
+    center = cmath.rect(c, centerAxisAngle)
+    #print("center position:",center)
+
+    #magnitude of exiting velocity, calculated from the conservation of angular momentum
+    exitDisplacement = specificAngMomentum / soi
+
+    #find anomaly at the end by plugging in r = soi and then flipping the result
+    exitAnom = np.arccos((semiMajor * (1 - eccen**2)-soi)/(eccen*soi))
+
+    #correct quadrant of arccos for exitAnom, using the sign of exitFlightPathAngle
+    exitFlightPathAngle = np.arctan(eccen * np.sin(exitAnom) / (1+eccen*np.cos(exitAnom)))
+    if (exitFlightPathAngle > 0 and exitAnom < 0) or (exitFlightPathAngle < 0 and exitAnom > 0):
+        exitAnom *= -1
+    exitAnom -= np.pi
+    exitAnom *= -1
+    #print("exitAnom:",exitAnom)
+
+    #calculate exit position by traversing (angularly) from r0 to rf
+    #and plug in r = roi
+    exitPos = cmath.rect(soi, actualAngle + trueAnom + exitAnom)
+    #print("exitPos",exitPos)
+    #direction of exitVel goes from center of hyperbola to exit position
+    cToExitPos = exitPos - center
+    #get exitVel
+    exitVel = cmath.rect(exitDisplacement, cmath.polar(cToExitPos)[1])
+    #print("exitVel",exitVel)
+
+    return [exitPos.real, exitPos.imag, exitVel.real, exitVel.imag]
+
 #target = planet to go (in string: e.g. "MARS" or "MARS BARYCENTER")
 #TIME = time of entrance (in datetime obj form)
 #state0 = position of entrance ： [x,y,vx,vy]
@@ -20,7 +107,7 @@ import numpy as np
 #returns exit state [x, y, vx, vy], or false if the spacecraft never leaves SoI
 #all distances are in km, all times are in s
 #step is default at 3 seconds per step, or else it is visibly inaccurate
-def getExitState(target, TIME, state0, step = timedelta(seconds=3)):
+def exitSim(target, TIME, state0, step = timedelta(seconds=1)):
     #os.chdir("./SPICE/")
 
     #constant storing the planet's basic info: [radius(km), gm, SOI radius(km)]
@@ -42,10 +129,10 @@ def getExitState(target, TIME, state0, step = timedelta(seconds=3)):
     #TODO: add crash detection?
     #simulation loop: quit if the spacecraft has went around a circle (2 pi plus a bit more)
     while dist<soi:
-        '''
+
         #for graphing, un-comment if you want to see graph
         posLog.append(pos)
-        '''
+
         #phase = angle of complex number, in radians
         angTemp = cmath.phase(pos)
         a = cmath.rect(-1*gm/(dist**2),angTemp)
@@ -71,7 +158,7 @@ def getExitState(target, TIME, state0, step = timedelta(seconds=3)):
                 return False
 
 
-    '''
+
     #for graphing, un-comment if you want to see graph
     x = []
     y = []
@@ -80,9 +167,15 @@ def getExitState(target, TIME, state0, step = timedelta(seconds=3)):
         y.append(i.imag)
     plt.scatter(x,y, marker='o')
     plt.show()
-    '''
+
 
     return [pos.real, pos.imag, vel.real, vel.imag]
+
+def findAngle (x, y):
+    temp = np.arccos(x/np.sqrt(x**2+y**2))
+    if y<0:
+        temp = 2 * np.pi - temp
+    return temp
 
 if __name__ == '__main__':
     os.chdir("./SPICE/")
@@ -92,4 +185,6 @@ if __name__ == '__main__':
     #state0 = position of entrance ： [x,y,vx,vy]
 
     #assume barycenter to be center of cartesian graph (0,0)
-    getExitState("EARTH BARYCENTER", datetime(2001,10,15),[7000,7000,-5 ,5])
+    getExitState("EARTH BARYCENTER", datetime(2001,10,15),[87000.0,-87000.0,0,-6])
+
+    #graphHyperbola("EARTH BARYCENTER", datetime(2001,10,15),[87000.0,-87000.0,0,-6])
