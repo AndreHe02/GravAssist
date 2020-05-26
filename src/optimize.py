d0d770692b2d8f82c5179c13dfb0b8fedf574215
LifEZH2O <andre.he@outlook.com>
2020-05-26 15:12:30 +0800

    implemtning greedy minimization by gradient decsent

--- src/optimize.py
+++ src/optimize.py
@@ -1,38 +1,52 @@
 import random
 import numpy as np
 
-#walk steepest path down to local minima
-def greedy_descent(f, ranges, steps, tolerance=10):
-    #random initialization
-    p = [lo + (hi-lo) * random.random() for lo, hi in ranges]
-    min_f_yet = f(*p)
+#walk steepest path down to minimal gridpoint
+def steep_descent(f, ranges, steps, tolerance, pinit, condition):
+    #random initialization until valid
+    if not pinit: 
+        p = [lo + (hi-lo) * random.random() for lo, hi in ranges]
+        while not condition(*p) or f(*p) == None: 
+            p = [lo + (hi-lo) * random.random() for lo, hi in ranges]
+    else: p = pinit
+        
     k = len(ranges)
     flat = 0
-    while flat < tolerance:
-        min_f, next_p = None, None       
+    fmin_yet = f(*p)
+    visited = [p]
+    
+    #descend until no lower gridpoint exists
+    while True:
+        print('DV', fmin_yet)
+        fmin, pnext = None, None       
         for idx in range(pow(3, k)):
-            new_p = p[:]
+            #generate new parameters
+            pnew = p[:]
             for j in range(k):
                 sign, idx = idx % 3, idx // 3
-                new_p[j] += (sign-1) * steps[j]
-                new_p[j] = max(ranges[j][0], min(ranges[j][1], new_p[j]))
-            val_f = f(*new_p)
-            if (min_f == None) or val_f < min_f: min_f, next_p = val_f, new_p            
-        if min_f > min_f_yet: return p
-        elif min_f == min_f_yet: flat += 1
-        min_f_yet, p = min_f, next_p
-    return p
+                pnew[j] += (sign-1) * steps[j]
+                pnew[j] = max(ranges[j][0], min(ranges[j][1], pnew[j]))
+            #evaluate new parameters
+            if condition and condition(*pnew) and pnew not in visited:
+                    fval = f(*pnew)
+                    if fval != None and (fmin == None or fval < fmin): fmin, pnext = fval, pnew            
+                    visited.append(pnew)
+        #return if no adjacent function value is smaller
+        if fmin == None or fmin >= fmin_yet: return p
+        fmin_yet, p = fmin, pnext
+    
 
 #descent in decaying step sizes,
 #giving increasingly accurate result
-def greedy_minimize(f, ranges, steps, iters=1, decay_factor=5, tolerance=10):
+def decaying_descent(f, ranges, steps, condition, iters, decay_factor, tolerance, pinit):
     ranges = np.array(ranges)
     steps = np.array(steps)
-    p = greedy_descent(f, ranges, steps)
-    for _ in range(iters):
+    p = steep_descent(f, ranges, steps, tolerance, pinit, condition)
+    for _ in range(1, iters):
         upper = p + (ranges[:, 1] - ranges[:, 0]) / decay_factor
         lower = p - (ranges[:, 1] - ranges[:, 0]) / decay_factor
         ranges = np.array([x for x in zip(lower, upper)])
         steps = steps / decay_factor
-        p = greedy_descent(f, ranges, steps, tolerance)
+        p = steep_descent(f, ranges, steps, tolerance, p, condition)
     return p
+        
