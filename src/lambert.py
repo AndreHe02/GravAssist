6aebce888265cb8379b4aa1b74966a2e6569ee58
LifEZH2O <andre.he@outlook.com>
2020-05-26 12:50:23 +0800

    optimizing lambert fit time constraint function

--- src/lambert.py
+++ src/lambert.py
@@ -3,19 +3,37 @@ from numpy.linalg import norm as norm
 from numpy import array as arr
 from datetime import timedelta
 
-def lambert_transfer(p1, p2, T, GM):
+def ternary_search(f, lo, hi, precision):
+    while abs(hi-lo)> precision:
+        left_third = (2*lo + hi) / 3
+        right_third = (lo + 2*hi) / 3
+        if f(left_third) > f(right_third): lo, hi = left_third, hi
+        else: lo, hi = lo, right_third
+    return (lo+hi) / 2
+
+def binary_search(f, lo, hi, target, up, precision):
+    if abs(hi-lo) < precision: return None
+    mid = (lo + hi) / 2
+    while abs(hi - lo) > precision:
+        if (f(mid) <= target):
+            if up: lo = mid
+            else: hi = mid
+        else:
+            if up: hi = mid
+            else: lo = mid
+        mid = (lo + hi) / 2
+    if abs(f(mid) - target) > timedelta(days=1): return None
+    return mid
+
+def lambert_transfer(p1, p2, T, GM, prec):
     #vectors/variables followed by R, H, E are:
     #R - in dimension reduced 2D vector space
     #H - in vector space defined by hyperbola axes
     #E - in vector space defined by ellipse axes
-    
+
     #keep p1 as shorter leg
-    mirrored = norm(p2) < norm(p1)
-    if mirrored:
-        temp = p1
-        p1 = p2
-        p2 = temp
-    
+    mirrored = norm(p2) < norm(p1)  
+    if mirrored: p1, p2 = p2, p1
     #compute dimension reduction matrix
     i = p1 / norm(p1)
     p2n = p2 / norm(p2)
@@ -28,14 +46,13 @@ def lambert_transfer(p1, p2, T, GM):
     Rinv = lambda v: np.matmul(Rinv_, v)
     p1R = R(p1)
     p2R = R(p2)
-    
     #compute hyperbola paremeters
     #defined according to lambert's method
     aH = abs(norm(p2R) - norm(p1R))/2
     dH = norm(p2R - p1R) / 2
     bH = np.sqrt(dH**2 - aH**2)
-    
-    def transfer_paths(lH):
+
+    def time_transfer(lH, solve=False):
         #compute f2=(x,y) with respect to hyperbola axes
         xH = aH + abs(lH)
         yH = bH * np.sqrt(xH**2/aH**2 - 1)
@@ -49,16 +66,16 @@ def lambert_transfer(p1, p2, T, GM):
         #to reduced 2D space
         if lH >= 0: f2R = iH * xH + jH * yH + (p1R + p2R) / 2
         else: f2R = iH * xH - jH * yH + (p1R + p2R) / 2
-
+        
         #compute elliptical orbit parameters
         aE = (norm(p1R) + norm(p1R - f2R)) / 2
         cE = norm(f2R) / 2
         bE = np.sqrt(aE**2 - cE**2)
-        TE = timedelta(seconds=((4 * np.pi**2 * aE**3 / GM) ** 0.5)[0])
-
+        TE = timedelta(seconds=((4 * np.pi**2 * aE**3 / GM) ** 0.5))
+        
         #compute time to transfer from p1 to p2
         #on computed elliptical orbit
-        ctR = f2R / 2        
+        ctR = f2R / 2
         r1R = p1R - ctR
         r2R = p2R - ctR
         #linear transformation from reduced 2D
@@ -72,9 +89,9 @@ def lambert_transfer(p1, p2, T, GM):
         R2E = np.linalg.inv(E2R)
         r1E = np.matmul(R2E, r1R)
         r2E = np.matmul(R2E, r2R)
+
         #calculate time by Keplar's area law
-        th = np.arccos(np.sum(r1E * r2E) / norm(r1E) / norm(r2E))
-        if np.isnan(th): th = 0
+        th = np.arccos(min(1, max(-1, np.sum(r1E * r2E) / norm(r1E) / norm(r2E))))
         if th > np.pi: th = np.pi - th
         AreaE = aE * bE * np.pi
         ActR = th / (2*np.pi) * AreaE
@@ -86,9 +103,16 @@ def lambert_transfer(p1, p2, T, GM):
         if norm(rrefR + prefR) > norm(rrefR): AfR = ActR - tctR + tfR
         else: AfR = ActR - tctR - tfR
         dt = AfR / AreaE * TE
+        if not solve: return dt, TE-dt
         
         #compute initial and final velocities
-        if mirrored: rhref = np.cross(r2E, r1E)
+        #get speed from energy equation
+        s1 = np.sqrt(GM * (2 / norm(p1) - 1/aE))
+        s2 = np.sqrt(GM * (2 / norm(p2) - 1/aE))
+        if mirrored: 
+            rhref = np.cross(r2E, r1E)
+            r1E, r2E = r2E, r1E
+            s1, s2 = s2, s1
         else: rhref = np.cross(r1E, r2E)
         v1E = np.cross(rhref, r1E)
         v2E = np.cross(rhref, r2E)
@@ -96,53 +120,24 @@ def lambert_transfer(p1, p2, T, GM):
         v2R = np.matmul(E2R, v2E)
         v1 = Rinv(v1R)
         v2 = Rinv(v2R)
-        s1 = np.sqrt(GM * (2 / norm(p1) - 1/aE))
-        s2 = np.sqrt(GM * (2 / norm(p2) - 1/aE))
         v1 = v1 / norm(v1) * s1
         v2 = v2 / norm(v2) * s2
-
-        #short path and long path
-        return [{'dt':dt, 'aE':aE, 'bE':bE, 'f2':Rinv(f2R), 'v1':v1, 'v2':v2, 'Rinv':Rinv},
-                {'dt':TE-dt, 'aE':aE, 'bE':bE, 'f2':Rinv(f2R), 'v1':-v1, 'v2':-v2, 'Rinv':Rinv}]
-
-    #find the lH value that yields a transfer path
-    #satisfying the time constraint T
-    def fit_time_constraint(tsf_func, lH_range):
+        return [{'v1':v1, 'v2':v2, 'dt':dt}, {'v1':-v1, 'v2':-v2, 'dt':TE-dt}]
         
-        def ternary_search(f, left, right, absolute_precision=10):
-            if abs(right - left) < absolute_precision:
-                return (left + right) / 2
-            left_third = (2*left + right) / 3
-            right_third = (left + 2*right) / 3
-            if f(left_third) > f(right_third):
-                return ternary_search(f, left_third, right, absolute_precision)
-            else:
-                return ternary_search(f, left, right_third, absolute_precision)
-
-        def binary_search(f, lo, hi, target, incr=True, precision=10):
-            if hi-lo < 1e-2 * lH_range: return None #no interval, stop search
-            mid = (lo + hi) / 2
-            while abs(hi - lo) > precision:
-                if (f(mid) <= target):
-                    if incr: lo = mid
-                    else: hi = mid
-                else:
-                    if incr: hi = mid
-                    else: lo = mid
-                mid = (lo + hi) / 2
-            if abs(f(mid) - target) > timedelta(days=1): return None
-            return mid
-
-        tsf_time = lambda lH: tsf_func(lH)['dt']
-        left_min = ternary_search(tsf_time, -lH_range, 0)
-        right_min = ternary_search(tsf_time, 0, lH_range)
-        valid_lHs = [binary_search(tsf_time, -lH_range, left_min, T, incr=False),
-            binary_search(tsf_time, left_min, 0, T, incr=True),
-            binary_search(tsf_time, 0, right_min, T, incr=False),
-            binary_search(tsf_time, right_min, lH_range, T, incr=True)]
-        return [tsf_func(lH) for lH in valid_lHs if lH]
-        
-    search_range= 1e9
-    short_solutions = fit_time_constraint(lambda lH: transfer_paths(lH)[0], search_range)
-    long_solutions = fit_time_constraint(lambda lH: transfer_paths(lH)[1], search_range)
+    #fit time constraint
+    def fit_time_constraint(tsf, lH_range=1e9):
+        left_min = ternary_search(tsf, -lH_range, 0, prec)
+        right_min = ternary_search(tsf, 0, lH_range, prec)
+        valid_lHs = [binary_search(tsf, -lH_range, left_min, T, False, prec),
+            binary_search(tsf, left_min, 0, T, True, prec),
+            binary_search(tsf, 0, right_min, T,False, prec),
+            binary_search(tsf, right_min, lH_range, T, True, prec)]
+        return [lH for lH in valid_lHs if lH]
+    
+    TSF1 = lambda lH: time_transfer(lH)[0]
+    TSF2 = lambda lH: time_transfer(lH)[1]
+    short_lHs = fit_time_constraint(TSF1)
+    long_lHs = fit_time_constraint(TSF2)
+    short_solutions = [time_transfer(lH, solve=True)[0] for lH in short_lHs]
+    long_solutions = [time_transfer(lH, solve=True)[1] for lH in long_lHs]  
     return short_solutions + long_solutions
--- src/optimize.py
+++ src/optimize.py
@@ -2,13 +2,13 @@ import random
 import numpy as np
 
 #walk steepest path down to local minima
-def greedy_descent(f, ranges, steps):
+def greedy_descent(f, ranges, steps, tolerance=10):
     #random initialization
     p = [lo + (hi-lo) * random.random() for lo, hi in ranges]
     min_f_yet = f(*p)
     k = len(ranges)
     flat = 0
-    while flat < 10:
+    while flat < tolerance:
         min_f, next_p = None, None       
         for idx in range(pow(3, k)):
             new_p = p[:]
@@ -25,7 +25,7 @@ def greedy_descent(f, ranges, steps):
 
 #descent in decaying step sizes,
 #giving increasingly accurate result
-def greedy_minimize(f, ranges, steps, iters=1, decay_factor=5):
+def greedy_minimize(f, ranges, steps, iters=1, decay_factor=5, tolerance=10):
     ranges = np.array(ranges)
     steps = np.array(steps)
     p = greedy_descent(f, ranges, steps)
@@ -34,5 +34,5 @@ def greedy_minimize(f, ranges, steps, iters=1, decay_factor=5):
         lower = p - (ranges[:, 1] - ranges[:, 0]) / decay_factor
         ranges = np.array([x for x in zip(lower, upper)])
         steps = steps / decay_factor
-        p = greedy_descent(f, ranges, steps)
+        p = greedy_descent(f, ranges, steps, tolerance)
     return p
